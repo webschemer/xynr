69ee83087a5fdeca05d7ca37d49f201768c121a0
ThemeSchemer <94884864+themeschemer@users.noreply.github.com>
2021-11-23 16:51:38 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+// Vue Ripple Components
 Vue.use(VueTouchRipple, {
-    speed: 1,
-    color: white,
-    opacity: 0.2
+	speed: 1,
+  color: '#000',
+  opacity: 0.2
 })
-
+Vue.component('cbtn', {
+  props: ['itemprop'],
+  template: '<touch-ripple :speed="1.5" color="#fff" class="btn"><button class="contained">{{itemprop}}</button></touch-ripple>'
+})
 new Vue({
-    el: '#vueapp',
-    data: {
-        // farding
-    },
+  el: '#vueapp',
+  data: {
+    // Can be used as inner html component
+  },
 })
